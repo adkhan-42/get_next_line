231b11f72c32e8bffb49c7db49a3a075dc53da6e
Adil Khan <adkhan@lab3r3s9.42abudhabi.ae>
2026-02-07 17:06:58 +0400

    Moulinette Approved Version

--- get_next_line.h
+++ get_next_line.h
@@ -13,9 +13,7 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdio.h>
 # include <stdlib.h>
-# include <string.h>
 # include <unistd.h>
 
 # ifndef BUFFER_SIZE
---
+++ gnl_bonus/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include "get_next_line_bonus.h"
+
+
+//int main(void)
+{
+    int fd;
+    char *ret;
+    fd =open("test.txt", O_RDONLY);
+    if (fd<0)
+    {
+        printf("ERROR BITCH GIVE ME A FILE TO READ NOT THIS BS");
+        return (0);
+    }
+    while ((ret = get_next_line_bonus(fd)) != NULL)
+    {
+        printf("The Line is [%s]",ret);
+        free(ret);
+    }
+    close (fd);
+    return (0);
+}
